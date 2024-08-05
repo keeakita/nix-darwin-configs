0006754863fbfded3dc55f170831da526ad4a099
Keea <keea@keea.dog>
2024-08-05 15:58:30 -0700

    initial commit

---
+++ darwin-configuration.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    neovim
+    git
+    openssh
+    chezmoi
+    coreutils
+    alacritty
+    curl
+    wget
+  ];
+
+  environment.shells = with pkgs; [
+    zsh
+  ];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
